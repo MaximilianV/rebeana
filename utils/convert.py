b32c6152615bb22fe22f5ffe463e2a5a76a0a70b
MaximilianV <maximilian-voelker@t-online.de>
2020-01-07 18:35:07 +0100

    Add module to convert a XES file to a parquet file

---
+++ utils/convert.py
@@ -0,0 +1,12 @@
+from pm4py.objects.log.importer.xes import factory as xes_importer
+from pm4py.objects.log.exporter.parquet import factory as parquet_exporter
+
+def convert_xes_file_to_parquet(xes_input_path: str, parquet_output_path: str):
+    """Converts a XES file at the given location to a parquet file (via pandas dataframes)
+
+    Arguments:
+        xes_input_path {str} -- The filepath the XES file should be read from
+        parquet_output_path {str} -- The filepath the new parquet file should be written to
+    """
+    log = xes_importer.apply(xes_input_path)
+    parquet_exporter.apply(log, parquet_output_path)
