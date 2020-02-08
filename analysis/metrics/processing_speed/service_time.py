7d9f85a55ab492807ae3520178f0edb01c143ba1
MaximilianV <maximilian-voelker@t-online.de>
2020-02-08 09:22:46 +0100

    Change processing time to be stored in seconds

--- analysis/metrics/processing_speed/service_time.py
+++ analysis/metrics/processing_speed/service_time.py
@@ -1,20 +1,22 @@
-import pandas as pd
+import numpy as np
 
 
 def activity_duration(group, column_name):
     group[column_name] = group["time:timestamp"] - group["time:timestamp"].shift(
-        periods=1).where(group["lifecycle:transition"].isin(["complete", "suspend"]), pd.NaT)
+        periods=1).where(group["lifecycle:transition"].isin(["complete", "suspend"]), np.nan)
+
+    group[column_name] = group[column_name].dt.total_seconds()
 
     return group
 
 
 def compute(log, resource: str, *, column_name: str = "proc_speed"):
-    log.loc[log["org:resource"].isin([resource]), column_name] = pd.NaT
+    log.loc[log["org:resource"].isin([resource]), column_name] = np.nan
 
     log.loc[log["org:resource"].isin([resource]) & log["lifecycle:transition"].isin(
         ["suspend", "complete", "start", "resume"])] = log.loc[log["org:resource"].isin([resource]) & log["lifecycle:transition"].isin(
         ["suspend", "complete", "start", "resume"])].groupby(["case:concept:name", "concept:name"]).apply(activity_duration, column_name)
 
-    log[column_name].fillna(value=pd.NaT, inplace=True)
+    log[column_name].fillna(value=np.nan, inplace=True)
 
     return log
