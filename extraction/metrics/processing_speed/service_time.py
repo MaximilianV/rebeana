ccaba30663baf978dab8f59f4634f62ef9e67bea
MaximilianV <maximilian-voelker@t-online.de>
2020-04-16 22:47:16 +0200

    Make lifecycle detection case insensitive

--- extraction/metrics/processing_speed/service_time.py
+++ extraction/metrics/processing_speed/service_time.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def activity_duration(group, column_name):
     group[column_name] = group["time:timestamp"] - group["time:timestamp"].shift(
-        periods=1).where(group["lifecycle:transition"].isin(["complete", "suspend"]), np.nan)
+        periods=1).where(group["lifecycle:transition"].str.lower().isin(["complete", "suspend"]), np.nan)
 
     group[column_name] = round(group[column_name].dt.total_seconds())
 
@@ -13,9 +13,13 @@ def activity_duration(group, column_name):
 def compute(log, resource: str, *, column_name: str = "proc_speed", max_time: int = None, min_time: int = None):
     log.loc[log["org:resource"].isin([resource]), column_name] = np.nan
 
-    log.loc[log["org:resource"].isin([resource]) & log["lifecycle:transition"].isin(
-        ["suspend", "complete", "start", "resume"])] = log.loc[log["org:resource"].isin([resource]) & log["lifecycle:transition"].isin(
-        ["suspend", "complete", "start", "resume"])].groupby(["case:concept:name", "concept:name"]).apply(activity_duration, column_name)
+    log.loc[
+        log["org:resource"].isin([resource]) &
+        log["lifecycle:transition"].str.lower().isin(["suspend", "complete", "start", "resume"])
+        ] = log.loc[
+            log["org:resource"].isin([resource]) &
+            log["lifecycle:transition"].str.lower().isin(["suspend", "complete", "start", "resume"])
+            ].groupby(["case:concept:name", "concept:name"]).apply(activity_duration, column_name)
 
     if max_time:
         log.loc[log[column_name] > max_time, column_name] = np.nan
