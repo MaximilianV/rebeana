366e3171604d54c42befcdef5a9b51469eb85c4f
MaximilianV <maximilian-voelker@t-online.de>
2020-06-01 14:20:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -16,13 +16,21 @@ from utils.Configuration import Configuration
 
 # from evaluation.Prediction import Prediction
 
-# dataset_path = os.path.join('/workspaces/data/BPIC-17',
-#                             'BPI_Challenge_2017.parquet')
-dataset_path = os.path.join('all_wl30min_psInSecMax7200_dt.parquet')
+dataset_path = os.path.join('/workspaces/data/BPIC-17',
+                            'BPI_Challenge_2017.parquet')
+# dataset_path = os.path.join('/workspaces/data/BPIC-12',
+                            # 'BPI_Challenge_2012.parquet')
+# dataset_path = os.path.join('all_wl30min_psInSecMax7200_dt.parquet')
+# dataset_path = os.path.join('top20_wl30min_psInSecMax7200_dt_bpi12.parquet')
 
-ALREADY_ANALYSED = True
+ALREADY_ANALYSED = False
 
 log = parquet_importer.apply(dataset_path)
+
+
+# print(log["concept:name"].value_counts())
+# print(get_resources(log, as_dict=True))
+# exit()
 
 OUTPUT_PATH = "results/"
 
@@ -33,11 +41,18 @@ if not ALREADY_ANALYSED:
     log.set_index('time:timestamp', inplace=True, drop=False)
     log.sort_index(inplace=True)
 
-########################################################################################################
 
-# Filter for Worklow Events only (Offer and Application do not have a duration)
+    # Filter for Worklow Events only (Offer and Application do not have a duration)
+    # BPIC-17
     log = log[(log["EventOrigin"] == "Workflow") & log["lifecycle:transition"].isin(
         ["suspend", "complete", "start", "resume"])]
+
+    # BPIC-12
+    # log = log[log["concept:name"].isin(['W_Completeren aanvraag', 'W_Afhandelen leads', 'W_Nabellen offertes', 'W_Beoordelen fraude', 'W_Valideren aanvraag', 'W_Nabellen incomplete dossiers', 'W_Wijzigen contractgegevens'])]
+# log = log[(log["case:RequestedAmount"] <= 50000)]
+#  & (log["proc_speed"] <= 2000) & (log["workload"] <= 60) & (log["proc_speed"] >= 120)
+
+# log = log[((log["proc_speed"] >= 1000))]
 
 
 ######################
@@ -46,21 +61,34 @@ if not ALREADY_ANALYSED:
 
 execution = Configuration('Test', log=log)
 
-execution.resources = get_most_frequent_resources(execution.log, 20)
-# execution.resources = ['User_87']
+# execution.resources = get_most_frequent_resources(execution.log, 20)
+execution.resources = ['User_9']
+# execution.resources = ['User_9', 'User_132', 'User_89', 'User_139']
 
+# BPI 12
+# execution.activities = ['W_Afhandelen leads']
+# execution.activities = ['W_Nabellen offertes']
+# execution.activities = ['W_Nabellen incomplete dossiers']
+# execution.activities = ['W_Completeren aanvraag']
+# execution.activities = ['W_Valideren aanvraag']
+# execution.activities = ['W_Nabellen incomplete dossiers']
+
+# BPI 17
 # execution.activities = ['W_Validate application']
-execution.activities = ['W_Complete application']
+# execution.activities = ['W_Complete application']
 # execution.activities = ['W_Call incomplete files']
+execution.activities = ['W_Call after offers']
+# execution.activities = ['W_Handle leads']
+# execution.activities = ['W_Call after offers','W_Call incomplete files','W_Complete application']
 
 # execution.input_metrics = ['Workload', 'Amount', 'Daytime']
-execution.input_metrics = ['Workload']
+execution.input_metrics = ['Workload', 'Daytime']
 execution.output_metrics = ['Processing Speed']
 execution.metric_configurations = {
     'Workload': {
         'variant': 'Eventsum',
         'configuration': {
-            'time_window': '30min'
+            'time_window': '3h'
         }
     },
     'Daytime': {
@@ -81,6 +109,8 @@ execution.metric_configurations = {
 }
 
 
+
+
 ######################
 ##### EXTRACTION #####
 ######################
@@ -89,21 +119,19 @@ if not ALREADY_ANALYSED:
     Extraction.extract_metrics(execution)
     # print(execution.log[execution.log["org:resource"].isin(execution.resources)][["time:timestamp", "case:concept:name", "concept:name", "lifecycle:transition", "org:resource", "daytime", "proc_speed", "workload"]].to_string())
 
+# print(log[log['org:resource'].isin(execution.resources)]['concept:name'].value_counts())
 
 ######################
 ###### ANALYSIS ######
 ######################
 
-# print(log[log['org:resource'].isin(execution.resources)]['concept:name'].value_counts())
-
-
+# CORRELATION
 correlation = Correlation(execution)
 correlation.compute_correlation()
-
 execution.correlation = correlation.result
 
+# REGRESSION
 regression = Regression(execution)
-
 execution.regression = regression.linear_regression()
 
 ###########################
@@ -116,8 +144,12 @@ visualiser.boxPlots()
 visualiser.visualiseCorrelation()
 visualiser.scatterPlots()
 
-print("##### CURRENT ID: ", execution.id)
 
+#################
+###### SAVE #####
+#################
+
+print("##### CURRENT ID: ", execution.id)
 execution.save_configuration()
 
 
